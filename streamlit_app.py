7191b20b09be2ca72d4e4391bec502b907e1549f
Ad <84122790+athibaut2017@users.noreply.github.com>
2023-01-25 14:02:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('Mon application streamlit')
 streamlit.header('Elle est super chouette')
 streamlit.text('Mais elle reste un peu basique')
-streamilt.text('Poulet 🐔')
+streamlit.text('Poulet 🐔')
