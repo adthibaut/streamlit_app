d1cedc04ab05d3169501072d46c7db30e70a4f56
Ad <84122790+athibaut2017@users.noreply.github.com>
2023-01-25 14:08:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title('Mon application streamlit')
 streamlit.header('Elle est super chouette')
 streamlit.text('Mais elle reste un peu basique')
 streamlit.text('Poulet 🐔')
+
+streamlit.title('Et maintenant une liste de fruits parce que pourquoi pas?')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
