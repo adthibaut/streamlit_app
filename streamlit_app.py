dc065dc6db721abf0ca5216aff5942f785c5d51a
Ad <84122790+athibaut2017@users.noreply.github.com>
2023-01-25 14:15:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,8 @@ streamlit.text('Mais elle reste un peu basique')
 streamlit.text('Poulet 🐔')
 
 streamlit.header('Et maintenant une liste de fruits parce que pourquoi pas?')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt", index = True)
 streamlit.dataframe(my_fruit_list)
+
+#pick in the list
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
